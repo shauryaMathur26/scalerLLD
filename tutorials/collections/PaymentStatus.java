06a929b13f7bb2ffb552cf5cc435f12498997f6b
Shaurya Mathur <47005444+ShauryaMathur@users.noreply.github.com>
2024-09-11 00:21:06 -0400

    Removed misplaced folders

--- tutorials/collections/PaymentStatus.java
+++
@@ -1,9 +0,0 @@
-package collections;
-
-public enum PaymentStatus {
-    SUCCESS,
-    FAILURE,
-    BANK_ERROR,
-    MERCHANT_ISSUE,
-    PENDING
-}
--- tutorials/lambdasAndStreams/Task.java
+++
@@ -1,8 +0,0 @@
-package lambdasAndStreams;
-
-public class Task implements Runnable{
-    @Override
-    public void run() {
-        System.out.println("Hi from Task Class");
-    }
-}
